8306aaab064153a99e11ae2664cd77b985648878
uiu-zishan <zishanmunna943@gmail.com>
2026-09-12 00:24:17 +0600

    fix(db): updated DSN to use port 3307 for XAMPP MySQL

--- config/db_connect.php
+++ config/db_connect.php
@@ -8,7 +8,7 @@ $user = 'root';
 $pass = ''; // Add your MySQL password here if you have one
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=3307;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
